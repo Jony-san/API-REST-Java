05f26c6329a22aed0558e2b103a2c5d9bcb7b32a
Jony-san <jonathan.san.mejia@gmail.com>
2026-02-18 17:14:29 -0600

    Add: dto de update de usuario

---
+++ users-api/src/main/java/com/chakray/usersapi/dto/UpdateUserRequest.java
@@ -0,0 +1,13 @@
+package com.chakray.usersapi.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateUserRequest {
+
+    private String email;
+    private String name;
+    private String phone;
+    private String password;
+    private String taxId;
+}
